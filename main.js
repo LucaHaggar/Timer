4464315a98000c2a528e148294cdf12279be869d
LucaHaggar <luca.haggar1@gmail.com>
2024-05-07 12:36:00 +0200

    1

--- main.js
+++ main.js
@@ -4,12 +4,18 @@ let elapsedTime = 0;
 
 function startTimer() {
   startTime = new Date();
+  localStorage.setItem("startTime", startTime.getTime());
 
   timerInterval = setInterval(updateTimer, 1000);
 }
 
 function updateTimer() {
   const currentTime = new Date();
+  const savedStartTime = localStorage.getItem("startTime");
+  if (savedStartTime) {
+    startTime = new Date(parseInt(savedStartTime));
+  }
+
   const currentTimeInSeconds = Math.floor(currentTime.getTime() / 1000);
   const startTimeInSeconds = Math.floor(startTime.getTime() / 1000);
   elapsedTime = currentTimeInSeconds - startTimeInSeconds;
@@ -36,41 +42,33 @@ function updateTimer() {
 
 function resetTimer() {
   clearInterval(timerInterval);
-  startTime = null;
+  localStorage.removeItem("startTime");
   elapsedTime = 0;
   document.getElementById("timer").textContent = "00:00:00";
-  localStorage.removeItem("startTime");
-  localStorage.removeItem("elapsedTime");
 }
 
 function padZero(num) {
   return num < 10 ? `0${num}` : num;
 }
 
-// Controlla se ci sono dati salvati nel localStorage
-const savedStartTime = localStorage.getItem("startTime");
-const savedElapsedTime = localStorage.getItem("elapsedTime");
-if (savedStartTime && savedElapsedTime) {
-  // Recupera l'orario di inizio e il tempo trascorso dal localStorage
-  startTime = new Date(savedStartTime);
-  elapsedTime = parseInt(savedElapsedTime);
-  // Avvia il timer con il tempo trascorso già memorizzato
-  startTimer();
-}
-
 document.getElementById("startButton").addEventListener("click", function () {
   startTimer();
-  // Salva l'orario di avvio nel localStorage
-  localStorage.setItem("startTime", new Date());
 });
 
 document.getElementById("resetButton").addEventListener("click", function () {
   resetTimer();
 });
 
+// Calcola il tempo trascorso all'avvio della pagina
+window.addEventListener("DOMContentLoaded", function () {
+  const savedElapsedTime = localStorage.getItem("elapsedTime");
+  if (savedElapsedTime) {
+    elapsedTime = parseInt(savedElapsedTime);
+    startTimer();
+  }
+});
+
 // Salva il tempo trascorso nel localStorage quando l'applicazione viene chiusa
 window.addEventListener("beforeunload", function () {
-  if (startTime) {
-    localStorage.setItem("elapsedTime", elapsedTime);
-  }
+  localStorage.setItem("elapsedTime", elapsedTime);
 });
